8a38411c2f27ca7a95f251bc9dc2127b4d2037b4
syiikin <49388427+syiikin@users.noreply.github.com>
2019-04-08 16:16:59 +0800

    Update dropify.js
    {{replace}} to {{ replace }}

--- src/js/dropify.js
+++ src/js/dropify.js
@@ -64,7 +64,7 @@
               wrap:            '<div class="dropify-wrapper"></div>',
               loader:          '<div class="dropify-loader"></div>',
               message:         '<div class="dropify-message"><span class="file-icon" /> <p>{{ default }}</p></div>',
-              preview:         '<div class="dropify-preview"><span class="dropify-render"></span><div class="dropify-infos"><div class="dropify-infos-inner"><p class="dropify-infos-message">{{replace}}</p></div></div></div>',
+              preview:         '<div class="dropify-preview"><span class="dropify-render"></span><div class="dropify-infos"><div class="dropify-infos-inner"><p class="dropify-infos-message">{{ replace }}</p></div></div></div>',
               filename:        '<p class="dropify-filename"><span class="dropify-filename-inner"></span></p>',
               clearButton:     '<button type="button" class="dropify-clear">{{ remove }}</button>',
               errorLine:       '<p class="dropify-error">{{ error }}</p>',
